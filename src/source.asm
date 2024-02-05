dc89588f9233d7280d26fa0b942e3ba20bd3499f
kbui1213 <159034261+kbui1213@users.noreply.github.com>
2024-02-05 15:53:52 +0000

    another msg

--- src/source.asm
+++ src/source.asm
@@ -15,11 +15,21 @@ INCLUDELIB user32.lib
 .data
     
 	; data declarations go here
+	variableA   SDWORD  ?               ; 32-bit signed integer
+    variableB   SDWORD  -3210           ; 16-bit signed integer
+    variableC   SDWORD  ?               ; 32-bit signed integer
+    variableD   SBYTE   ?               ; 8-bit signed integer
+
+promptA 	BYTE "What is value of A?", 0
 
 .code
 main PROC
 	
 	; code goes here
+	mov edx, OFFSET promptA
+    call WriteString
+    call ReadInt
+    mov variableA, eax
 	call DumpRegs ; displays registers in console
 
 	exit
